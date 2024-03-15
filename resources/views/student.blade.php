e94be0eb9e39420d3889a40dafba23cddcbd6e58
Meet-Bhalodia <109055372+Meet-Bhalodia@users.noreply.github.com>
2024-03-15 23:03:39 +0530

    first

---
+++ resources/views/student.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.app')
+
+@section('title')
+    student
+@endsection
+
+@section('mainTitle')
+    mainTitle
+@endsection
+
+@section('breadCrumb')
+    
+@endsection
+
+@section('mainContent')
+    
+@endsection
